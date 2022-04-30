43cf591410cad57eda1ec2eaa9958c0049894a21
manchala meherish <manchala.meherish1524@gmail.com>
2022-04-30 23:54:57 +0530

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,6 +1,6 @@
 public class GUI {
 
 	public static void main(String[] args) {
-	        JFrameWindow window = new JFrameWindow(00, 500);
+	        JFrameWindow window = new JFrameWindow(300, 500);
 	}
 }
